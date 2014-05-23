2bd2838e0d819ac17f59cc1f5bdf4cfe6e675263
Dave Lane <davidlane2@mac.com>
2014-05-23 09:12:01 -0700

    Changed servce get repoonse to expect an object for a project

--- app/js/services/projectService.js
+++ app/js/services/projectService.js
@@ -1,30 +1,10 @@
 'use strict';
 
-// angular.module('myApp')
-//   .factory('ProjectService', [ '$resource', function($resource) {
-
-//     return $resource('projects/:projectId.json', {}, {
-//       fetchOne: {method:'GET', isArray:true},
-//       fetchAll: {method:'GET', params:{projectId: 'projects'}, isArray:true}
-//     });
-
-//   }]);
-
-
-// angular.module('myApp')
-//   .factory('ProjectService',
-//     function($resource) {
-//       return $resource('projects/projects.json');
-//     }
-//   );
-
-
 angular.module('myApp')
   .factory('projectService',
     function($resource) {
       return $resource('projects/:projectId.json', {}, {
-        query: {params: {projectId: 'projects'}, isArray: true},
-        get: {isArray: true}
+        query: {params: {projectId: 'projects'}, isArray: true}
       });
     }
   );
